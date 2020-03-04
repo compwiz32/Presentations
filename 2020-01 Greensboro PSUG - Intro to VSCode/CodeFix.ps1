09b6b89263e7c0c5100feb9a9319161b94fa43ad
Mike Kanakos <mkanakos@gmail.com>
2020-03-04 12:17:39 -0500

    remove file

--- 2020-01 Greensboro PSUG - Intro to VSCode/CodeFix.ps1
+++
@@ -1,8 +0,0 @@
-Function Prompt
-{
-"PS $($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1)) "
-[console]::beep(2000,500)
-# .Link# http://go.microsoft.com/fwlink/?LinkID=225750
-# .ExternalHelp System.Management.Automation.dll-help.xml
-
-}
