7495b016c4059414cc12b47afd64b4f8826f28c1
compwiz32 <mkanakos@gmail.com>
2020-12-07 21:26:11 -0500

    more demos

--- 2020-12 MSPSUG Secrets Mgmt Demo/!Presentation.ps1
+++ 2020-12 MSPSUG Secrets Mgmt Demo/!Presentation.ps1
@@ -26,21 +26,52 @@ $UserModuleFolder =
 # doc Link
 https://devblogs.microsoft.com/powershell/secretmanagement-preview-6-and-secretstore-preview-4/
 
-#Highlevel plan
+#passwords in PowerShell the old way!
+$oldcred = Get-credential mklab\mkana
 
-#Install Modules
+# Extract the password - insecure
+$oldcred.Password | ConvertFrom-SecureString -AsPlainText
+
+# a better way - secrets mgmt
+
+# Install Secret Mgmt Module (the engine)
 Install-Module -Name Microsoft.PowerShell.SecretManagement -AllowPrerelease -Repository PSGallery
+
+# Install Secret Management Store (the translator & storage)
 Install-Module -Name Microsoft.PowerShell.SecretStore -AllowPrerelease -Repository PSGallery
 
+#PowerShellv5
+Install-Module PowerShellGet -Allow Clobber
+# logoff PS Session then relaunch and install Secrets Modules
+
 #Build a vault
+Register-SecretVault -Name VaultDemo -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault -AllowClobber
+
+# Where is the vault password? Not required to build vault
 
 #add a Secret
-
-#add a PSCredential Object
+set-secret TextString -Secret "This is just some text"
+Set-Secret APIKey -Secret "c6854da1-743d-4234-bdd0-3f9832185b01"
 
 #recall a secret
+Get-Secret TextString
 
 #save to a variable
+$a = Get-Secret TextString
+
+
+#add a PSCredential Object
+Set-Secret -name CredMK -Secret (Get-pscredential)
+
+#dont make this mistake
+Set-Secret (Get-pscredential)
+
+
+
+
+
+
+
 
 # call a secret on the fly
 
